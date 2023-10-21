ad15c179bb238708857bcaf5f18ab13803775dd5
Vasavi Kotipalli <vkotipalli2@huskers.unl.edu>
2023-10-21 16:21:25 -0500

    touch up

--- src/components/Challenge.js
+++ src/components/Challenge.js
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
 import '../styles/Challenge.css'
-// import BookSelector from './BookSelector';
 import BookList from '../data/BookList';
 import ProgressBar from './Progress';
-// import BookList from './BookList'; 
-// Import your book data
 
 const Challenge = () => {
   const [goal, setGoal] = useState(0);
@@ -38,59 +35,3 @@ const Challenge = () => {
 }
 
 export default Challenge;
-
-// const Challenge = (props) => {
-  
-//     return(
-//         <div className='challenge'>
-//             <h3>Reading Challenge</h3>
-            
-//         </div>
-//     );
-// };
-// export default Challenge;
-
-
-// function Challenge() {
-//     const [goal, setGoal] = useState(0);
-//     const [progress, setProgress] = useState(0);
-//     const [selectedBook, setSelectedBook] = useState('');
-
-//     const handleSetGoal = (newGoal) => {
-//         setGoal(newGoal);
-//     };
-
-//     const handleAddProgress = (book) => {
-//         setProgress(progress + book.pageLength);
-//         setSelectedBook(book.title);
-//     };
-
-//   return (
-//     <div className="App">
-//       <h1>Reading Challenge</h1>
-//       <div>
-//         <div className="progress-bar">
-//           <div
-//             className="progress"
-//             style={{ width: `${(progress / goal) * 100 || 0}%` }}
-//           />
-//         </div>
-//         <p>
-//           Progress: {progress} pages out of {goal} pages (
-//           {goal === 0 ? 0 : ((progress / goal) * 100).toFixed(2)}%)
-//         </p>
-//         <input
-//           type="number"
-//           placeholder="Set Your Annual Reading Goal"
-//           onChange={(e) => handleSetGoal(e.target.value)}
-//         />
-//       </div>
-//       <BookSelector bookList={BookList} addProgress={handleAddProgress} />
-//       <div>
-//         {selectedBook && <p>Selected Book: {selectedBook}</p>}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Challenge;
